c7418cfbc976855dc074a128e3e971102153bc52
rightgo09 <skyarrow09@gmail.com>
2014-03-09 22:16:44 +0900

    Clean up skelton test

--- spec/zoho_crm_spec.rb
+++ spec/zoho_crm_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe ZohoCrm do
   it 'should have a version number' do
-    ZohoCrm::VERSION.should_not be_nil
-  end
-
-  it 'should do something useful' do
-    false.should eq(true)
+    expect(ZohoCrm::VERSION).to_not be_nil
   end
 end
