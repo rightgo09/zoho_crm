b70bb4f54386403793032da66c5c0f91ff8fa10a
rightgo09 <skyarrow09@gmail.com>
2014-03-11 00:43:31 +0900

    build_query method

--- lib/zoho_crm/util.rb
+++ lib/zoho_crm/util.rb
@@ -12,11 +12,24 @@ module ZohoCrm::Util
   end
 
   def build_url(action)
-    "https://crm.zoho.com/crm/private/json/#{self.to_s.demodulize}s/#{action}"
+    "https://crm.zoho.com/crm/private/json/#{zoho_module_name}/#{action}"
   end
 
   def build_query(params)
-    params.merge(authtoken: ZohoCrm.token, scope: "crmapi")
+    query = Hash[params.map { |k, v| [k.to_s.camelize(:lower), v] }].merge(
+      "authtoken" => ZohoCrm.token,
+      "scope" => "crmapi",
+    )
+
+    if query["selectColumns"].present? && query["selectColumns"].is_a?(Array)
+      query["selectColumns"] = zoho_module_name+"("+query["selectColumns"].join(",")+")"
+    end
+
+    query
+  end
+
+  def zoho_module_name
+    self.to_s.demodulize.pluralize
   end
 
 end
