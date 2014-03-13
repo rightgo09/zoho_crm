570067c486ad2585506633c0f3e83821390402d5
rightgo09 <skyarrow09@gmail.com>
2014-03-13 23:01:46 +0900

    add test of #build_url

--- spec/zoho_crm_spec.rb
+++ spec/zoho_crm_spec.rb
@@ -5,3 +5,14 @@ describe ZohoCrm do
     expect(ZohoCrm::VERSION).to_not be_nil
   end
 end
+
+describe ZohoCrm::Util do
+  let(:z) { Class.new { extend ZohoCrm::Util } }
+
+  describe "#build_url" do
+    before { ZohoCrm.const_set("BlackCoffee", z) }
+    it "should build url" do
+      expect(z.build_url(:get_my_records)).to eq("https://crm.zoho.com/crm/private/json/BlackCoffees/getMyRecords")
+    end
+  end
+end
