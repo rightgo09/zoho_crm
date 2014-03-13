496e62080f1d5bc6d395de9f77c175e8e983cc59
rightgo09 <skyarrow09@gmail.com>
2014-03-13 23:28:23 +0900

    add test of #check_token

--- spec/zoho_crm_spec.rb
+++ spec/zoho_crm_spec.rb
@@ -15,4 +15,22 @@ describe ZohoCrm::Util do
       expect(z.build_url(:get_my_records)).to eq("https://crm.zoho.com/crm/private/json/BlackCoffees/getMyRecords")
     end
   end
+
+  describe "#check_token" do
+    context "when token is not set" do
+      before { ZohoCrm.token = nil }
+      it "should raise RuntimeError" do
+        expect { z.check_token }.to raise_error(RuntimeError)
+      end
+    end
+
+    context "when token is set" do
+      let(:token) { "hogehogehoge" }
+      before { ZohoCrm.token = token }
+      it "should not raise RuntimeError" do
+        expect { z.check_token }.not_to raise_error
+      end
+    end
+  end
+
 end
